ffed08222f497f8b1b6c13f3e7990da7a97c6613
Rio723x <aizendrio@gmail.com>
2026-07-12 16:46:25 +0530

    Add: longest-common-subsequence - submission-1

---
+++ Data Structures & Algorithms/longest-common-subsequence/submission-1.py
@@ -0,0 +1,36 @@
+class Solution:
+    def helper( self , i , j, text1 , text2 , dp ):
+        if i == len(text1) or j == len(text2):
+            return 0
+        if i > len(text1) or j > len(text2):
+            return 0
+
+        if dp[i][j] != -1:
+            return dp[i][j]
+
+        #check
+        if text1[i] == text2[j]:
+            #dp[i][j] = 1 + self.helper(i+1,j+1,text1,text2,dp)
+            ways = 1 + self.helper(i+1,j+1,text1,text2,dp)
+
+        if text1[i] != text2[j]:
+            
+            ways = max(self.helper(i+1,j,text1,text2,dp), self.helper(i,j+1,text1,text2,dp))
+        
+         
+        dp[i][j] = ways
+        return dp[i][j]
+
+    def longestCommonSubsequence(self, text1: str, text2: str) -> int:
+        dp = [[0]* (len(text2)+1) for _ in range(len(text1)+1)]
+
+        for i in range(len(text1) - 1 , -1 , -1 ):
+            for j in range(len(text2) - 1  , -1 , -1):
+                if text1[i] == text2[j]:
+                    dp[i][j] = 1 + dp[i+1][j+1]
+
+                if text1[i] != text2[j]:
+                    dp[i][j] = max(dp[i+1][j] , dp[i][j+1])
+        
+        return dp[0][0]
+        
